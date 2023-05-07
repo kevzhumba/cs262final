08328cb846e4909fa7031d87ba966a9f667183e7
howardg2000 <hjguo@college.harvard.edu>
2023-05-07 14:16:55 -0400

    Add unit tests for machine

--- src/test/scala/dataflow/MachineTest.scala
+++ src/test/scala/dataflow/MachineTest.scala
@@ -3,6 +3,7 @@ package dataflow
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.flatspec.AnyFlatSpec
 import org.mockito.Mockito._
+import org.mockito.ArgumentMatchers.any
 import cfg.{
   BBNode,
   MethodDescription,
@@ -23,16 +24,18 @@ import org.opalj.br.{ObjectType, VirtualMethod}
 import org.json4s._
 import org.json4s.native.Serialization
 import org.json4s.native.Serialization.{read, write}
-import cfg.ExNode
-import cfg.ReturnValue
-import cfg.BBEntryNode
-import cfg.ExplodedCfg
-import protos.dataflow.DataflowServerGrpc
-import protos.dataflow.GetCallerInfoResponse
-import analyses.{Analysis, ConstantPropagationAnalysis}
+import cfg.{ExNode, ReturnValue, BBEntryNode, ExplodedCfg, CRNode}
+import protos.dataflow.{
+  GetCallerInfoResponse,
+  GetCalleeInfoResponse,
+  DataflowServerGrpc
+}
 
 class MachineSpec extends AnyFlatSpec with Matchers {
   val machine = new Machine("localhost", 1234)
+  implicit val formats = machine.formats
+
+  // Create example nodes for example cfg
   val exampleBBEntryNode = BBEntryNode(
     MethodDescription(
       "espTest.Foo",
@@ -71,127 +74,46 @@ class MachineSpec extends AnyFlatSpec with Matchers {
     ),
     15
   )
-  val cfg = ExplodedCfg(
+  val exampleExNode = ExNode(
+    false,
+    MethodDescription(
+      "espTest.Foo",
+      2,
+      ObjectType("espTest / Foo"),
+      false
+    ),
+    List(),
+    16
+  )
+  val exampleExNode2 = ExNode(
+    true,
+    MethodDescription(
+      "espTest.Foo",
+      2,
+      ObjectType("espTest / Foo"),
+      false
+    ),
+    List(),
+    17
+  )
+
+  val exampleCfg = ExplodedCfg(
     exampleBBEntryNode,
     Set(
-      BBEntryNode(
-        MethodDescription(
-          "espTest.Foo",
-          2,
-          ObjectType("espTest / Foo"),
-          false
-        ),
-        List(
-          StmtNode(
-            Assignment(
-              Var(List(0)),
-              BinExpr(Var(List(-2)), BinOps.Add, Var(List(-3)))
-            ),
-            MethodDescription(
-              "espTest.Foo",
-              2,
-              ObjectType("espTest / Foo"),
-              false
-            ),
-            false,
-            15,
-            0
-          ),
-          StmtNode(
-            ReturnValue(Var(List(0))),
-            MethodDescription(
-              "espTest.Foo",
-              2,
-              ObjectType("espTest / Foo"),
-              false
-            ),
-            false,
-            15,
-            1
-          )
-        ),
-        15
-      ),
-      ExNode(
-        false,
-        MethodDescription(
-          "espTest.Foo",
-          2,
-          ObjectType("espTest / Foo"),
-          false
-        ),
-        List(),
-        16
-      ),
-      ExNode(
-        true,
-        MethodDescription(
-          "espTest.Foo",
-          2,
-          ObjectType("espTest / Foo"),
-          false
-        ),
-        List(),
-        17
-      )
+      exampleBBEntryNode,
+      exampleExNode,
+      exampleExNode2
     ),
     Map(
       15 -> Set(),
       16 -> Set(),
       17 -> Set(
-        BBEntryNode(
-          MethodDescription(
-            "espTest.Foo",
-            2,
-            ObjectType("espTest / Foo"),
-            false
-          ),
-          List(
-            StmtNode(
-              Assignment(
-                Var(List(0)),
-                BinExpr(Var(List(-2)), BinOps.Add, Var(List(-3)))
-              ),
-              MethodDescription(
-                "espTest.Foo",
-                2,
-                ObjectType("espTest / Foo"),
-                false
-              ),
-              false,
-              15,
-              0
-            ),
-            StmtNode(
-              ReturnValue(Var(List(0))),
-              MethodDescription(
-                "espTest.Foo",
-                2,
-                ObjectType("espTest / Foo"),
-                false
-              ),
-              false,
-              15,
-              1
-            )
-          ),
-          15
-        )
+        exampleBBEntryNode
       )
     ),
     Map(
       15 -> Set(
-        ExNode(
-          true,
-          MethodDescription(
-            "espTest.Foo",
-            2,
-            ObjectType("espTest / Foo"),
-            false
-          ),
-          List(),
-          17
-        )
+        exampleExNode2
       ),
       16 -> Set(),
       17 -> Set()
@@ -202,52 +124,109 @@ class MachineSpec extends AnyFlatSpec with Matchers {
         MethodDescription("espTest.Main", 1, ObjectType("espTest / Main"), true)
       )
     ),
-    List()
+    List(
+      (
+        1,
+        List(
+          MethodDescription(
+            "espTest.Foo",
+            2,
+            ObjectType("espTest / Foo"),
+            false
+          )
+        )
+      )
+    )
   )
-  machine.cfg = cfg
+
+  // Initialize cfg and machine data structures, mock grpc stubs
+  machine.cfg = exampleCfg
   val mockStub = mock(classOf[DataflowServerGrpc.DataflowServerBlockingStub])
-  val latticeElem =
-    machine.valueOperator.getArgs(machine.out.get(exampleBBEntryNode))
+  val latticeElem = Map() +
+    ("1" -> (Integer(false, Some(5)), AbstractObject(Set())))
   val json = write(latticeElem)
-  when(mockStub.getCallerInfo).thenReturn(
+  when(mockStub.getCallerInfo(any())).thenReturn(
     GetCallerInfoResponse(json)
   )
+  when(mockStub.getCalleeInfo(any())).thenReturn(
+    GetCalleeInfoResponse(json)
+  )
+  val exampleMethodDescription = MethodDescription(
+    "espTest.Main",
+    1,
+    ObjectType("espTest / Main"),
+    true
+  )
   machine.methodStubs = Map(
+    exampleMethodDescription -> mockStub,
     MethodDescription(
-      "espTest.Main",
-      1,
-      ObjectType("espTest / Main"),
-      true
+      "espTest.Foo",
+      2,
+      ObjectType("espTest / Foo"),
+      false
     ) -> mockStub
   )
+  machine.method = exampleMethodDescription
+  machine.in.put(exampleBBEntryNode, Constant(latticeElem))
+  machine.out.put(exampleBBEntryNode, Constant(latticeElem))
 
-  "computeInForBBEntryNode" should "return the correct input for a BBNode" in {
-
+  "computeInForBBEntryNode" should "return the correct input for a BBENode" in {
     val node = exampleBBEntryNode
     val result = machine.computeInForBBEntryNode(node)
-    result shouldEqual 1
+    result shouldEqual Constant(latticeElem)
   }
 
-//   "computeInForCrNode" should "return the correct input for a CRNode" in {
-//     val callNode =
-//       BBNode(1, List(Instruction("const", "a", "1")), List(), List())
-//     val crNode =
-//       CRNode(2, List(Instruction("call", "foo", "3")), callNode, List())
-//     machine.in.put(callNode, Value.makeInteger(1))
-//     machine.cfg =
-//       new ExplodedCfg(List(callNode, crNode), List(), List(), Map(), Map())
-//     machine.cfg._callees = Map(3 -> List(MethodDescription("foo", List())))
-//     val result = machine.computeInForCrNode(crNode)
-//     result shouldEqual Value.makeInteger(0)
-//   }
+  "computeInForCrNode" should "return the correct input for a CRNode" in {
+    val node = CRNode(
+      MethodDescription(
+        "espTest.Foo",
+        2,
+        ObjectType("espTest / Foo"),
+        false
+      ),
+      List(
+        StmtNode(
+          ReturnValue(Var(List(0))),
+          MethodDescription(
+            "espTest.Foo",
+            2,
+            ObjectType("espTest / Foo"),
+            false
+          ),
+          false,
+          15,
+          1
+        )
+      ),
+      17
+    )
+    val result = machine.computeInForCrNode(node)
+    result shouldEqual Constant(
+      Map(
+        "1" -> (Integer(false, Some(5)), AbstractObject(Set())),
+        "returnFromCallee" -> (Integer(true, None), AbstractObject(Set()))
+      )
+    )
+  }
 
-//   "computeInForExitNode" should "return the correct input for an ExNode" in {
-//     val node1 = BBNode(1, List(Instruction("const", "a", "1")), List(), List())
-//     val node2 = BBNode(2, List(Instruction("const", "b", "2")), List(), List())
-//     val exitNode = ExNode(3, List(), List(node1, node2))
-//     machine.out.put(node1, Value.makeInteger(1))
-//     machine.out.put(node2, Value.makeInteger(2))
-//     val result = machine.computeInForExitNode(exitNode)
-//     result shouldEqual Value.makeInteger(2)
-//   }
+  "computeInForExitNode" should "return the correct input for an ExNode" in {
+    val node = exampleExNode2
+    val result = machine.computeInForExitNode(node)
+    result shouldEqual Constant(Map())
+  }
+
+  "computeInForBBNode" should "return the correct input for an BBNode" in {
+    val node = BBNode(
+      MethodDescription(
+        "espTest.Foo",
+        2,
+        ObjectType("espTest / Foo"),
+        false
+      ),
+      List(),
+      17
+    )
+    val result = machine.computeInForBBNode(node)
+    result shouldEqual Constant(latticeElem)
+  }
 }
